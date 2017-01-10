1ba1a09e237d56021868712384c96968d6af9690
Paul Kim <pkayokay@gmail.com>
2017-01-10 18:23:00 -0500

    path variable and params hash notes

--- app.rb
+++ app.rb
@@ -1,25 +1,35 @@
 require "sinatra"
 
 get "/" do
-  'You requested the root path!'  
+  "You requested the root path!"
 end
 
 get "/about" do
-  'This is the about path.'  
+  "This is the about path."
 end
 
 get "/roll" do
   "Your die roll is #{rand(1...7)}"
 end
 
-get “/greet/:name” do
+
+# DEFINING ROUTES WITH VARIABLES
+# path variable with a colon
+# Sinatra takes route variables and stores them in a hash, a PARAMS HASH
+# it constructs this Hash every time it receives a request, filling it with the incoming request
+# params[:variable_name]
+get "/greet/:name" do
   name = params[:name].capitalize
   "Hello there #{name}!"
 end
 
 get "/square/:number" do
-number = params[:number].to_i
-squared = number * number
+  number = params[:number].to_i
+  squared = number * number
+  "#{number} squared is #{squared}!"
+end
 
-"#{squared}"
+# multiple path variables
+get "/greet/:first/:last" do
+    "Hello there #{params[:first]} #{params[:last]}!"
 end
